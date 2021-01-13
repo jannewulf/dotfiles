f35302fadb2a9970d88e831b80a75b844ab519c2
Janne Wulf <jannewulf@gmail.com>
2021-01-13 21:59:56 +0100

    Prepare for use with local dotfiles repo

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# NOTE: Must be executed from dotfiles directory
+# NOTE: Must be executed from local dotfiles directory
 
-ln --symbolic --interactive --verbose configs/profile "$HOME"/.profile
+ln --symbolic --interactive --verbose dotfiles.global/configs/profile "$HOME"/.profile
 
-ln --symbolic --interactive --verbose configs/bash_profile "$HOME"/.bash_profile
-ln --symbolic --interactive --verbose configs/bashrc "$HOME"/.bashrc
-ln --symbolic --interactive --verbose configs/bash_aliases "$HOME"/.bash_aliases
+ln --symbolic --interactive --verbose dotfiles.global/configs/bash_profile "$HOME"/.bash_profile
+ln --symbolic --interactive --verbose dotfiles.global/configs/bashrc "$HOME"/.bashrc
+ln --symbolic --interactive --verbose dotfiles.global/configs/bash_aliases "$HOME"/.bash_aliases
 
-ln --symbolic --interactive --verbose configs/gitconfig "$HOME"/.gitconfig
+ln --symbolic --interactive --verbose dotfiles.global/configs/gitconfig "$HOME"/.gitconfig
 
-ln --symbolic --interactive --verbose configs/tmux.conf "$HOME"/.tmux.conf
+ln --symbolic --interactive --verbose dotfiles.global/configs/tmux.conf "$HOME"/.tmux.conf
 
-ln --symbolic --interactive --verbose configs/vimrc "$HOME"/.vimrc
+ln --symbolic --interactive --verbose dotfiles.global/configs/vimrc "$HOME"/.vimrc

0b5837280f1ef645053881d5a6448e8012975557
Janne Wulf <jannewulf@gmail.com>
2021-01-13 23:05:46 +0100

    Source local aliases

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -2,14 +2,14 @@
 
 # NOTE: Must be executed from local dotfiles directory
 
-ln --symbolic --interactive --verbose dotfiles.global/configs/profile "$HOME"/.profile
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/profile "$HOME"/.profile
 
-ln --symbolic --interactive --verbose dotfiles.global/configs/bash_profile "$HOME"/.bash_profile
-ln --symbolic --interactive --verbose dotfiles.global/configs/bashrc "$HOME"/.bashrc
-ln --symbolic --interactive --verbose dotfiles.global/configs/bash_aliases "$HOME"/.bash_aliases
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/bash_profile "$HOME"/.bash_profile
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/bashrc "$HOME"/.bashrc
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/bash_aliases "$HOME"/.bash_aliases
 
-ln --symbolic --interactive --verbose dotfiles.global/configs/gitconfig "$HOME"/.gitconfig
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/gitconfig "$HOME"/.gitconfig
 
-ln --symbolic --interactive --verbose dotfiles.global/configs/tmux.conf "$HOME"/.tmux.conf
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/tmux.conf "$HOME"/.tmux.conf
 
-ln --symbolic --interactive --verbose dotfiles.global/configs/vimrc "$HOME"/.vimrc
+ln --symbolic --interactive --verbose $(pwd)/dotfiles.global/configs/vimrc "$HOME"/.vimrc

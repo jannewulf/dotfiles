eb0691542c08820a28caf536f53f919bca2e16c8
Janne Wulf <jannewulf@gmail.com>
2021-01-13 20:05:57 +0100

    Change relative paths for linking

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-ln --symbolic --interactive --verbose ../configs/profile "$HOME"/.profile
+# NOTE: Must be executed from dotfiles directory
 
-ln --symbolic --interactive --verbose ../configs/bash_profile "$HOME"/.bash_profile
-ln --symbolic --interactive --verbose ../configs/bashrc "$HOME"/.bashrc
-ln --symbolic --interactive --verbose ../configs/bash_aliases "$HOME"/.bash_aliases
+ln --symbolic --interactive --verbose configs/profile "$HOME"/.profile
 
-ln --symbolic --interactive --verbose ../configs/gitconfig "$HOME"/.gitconfig
+ln --symbolic --interactive --verbose configs/bash_profile "$HOME"/.bash_profile
+ln --symbolic --interactive --verbose configs/bashrc "$HOME"/.bashrc
+ln --symbolic --interactive --verbose configs/bash_aliases "$HOME"/.bash_aliases
 
-ln --symbolic --interactive --verbose ../configs/tmux.conf "$HOME"/.tmux.conf
+ln --symbolic --interactive --verbose configs/gitconfig "$HOME"/.gitconfig
 
-ln --symbolic --interactive --verbose ../configs/vimrc "$HOME"/.vimrc
+ln --symbolic --interactive --verbose configs/tmux.conf "$HOME"/.tmux.conf
+
+ln --symbolic --interactive --verbose configs/vimrc "$HOME"/.vimrc
